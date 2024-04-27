9fdf53fe562dc3e2aeeee9240336e581ce67ff0a
mbreeden0007 <157175420+mbreeden0007@users.noreply.github.com>
2024-04-27 12:25:45 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Author: Matthew Breeden
+    File Name: scripts.js
+    Today's Date: 4/24/2024
+*/
+$(document).ready(function(){
+	$("#slideToggleButton").click(function(){
+		$(selector).datetimeEntry({datetimeFormat: 'O-D H:M'});
+});
